fb609799c881bb4cf5869cc2bbb4ee4359207eb0
JESSELLEM CIPRIANO <jessellem_cipriano@hotmail.com>
2022-04-30 10:58:47 -0300

    Exercícios de hash

--- Hash/index.js
+++ Hash/index.js
@@ -6,7 +6,6 @@ let wordsToHash = ["Luiza", "Luna", "Bia", "Luana"];
 function getCharCode(str, n) {
     let i, tamS = str.length;
     let hash = 0;
-
     for (i = 0; i < tamS; i++) {
         hash += str[i].charCodeAt();
     }
@@ -17,29 +16,22 @@ function getCharCode(str, n) {
 function createHash(word) {
     let Key = "maeToNoLuizaCode"
     let secondKey = "magaluArrasa"
-    
     const wordLength = word.length
-    
     Key = getCharCode(Key.split(''), wordLength)
-    
     secondKey = getCharCode(secondKey.split(''), wordLength)
-    
     const newWordValue = getCharCode(word.split(''), wordLength)
-    
-    let hash = Math.floor((Key + secondKey + newWordValue) / (word.length * 0.1))
-   
+    let hash = Math.floor((Key + secondKey + newWordValue) / (wordLength * 0.1))
     while (arrayHash[hash] !== undefined) {
         hash++;
     }
-
     return hash
 }
 
- wordsToHash.forEach(function(value, index) {
-     let indexHash = createHash(value);
-     console.log(value + ': ' + indexHash)
-     arrayHash[indexHash] = value;      
- });
+wordsToHash.forEach(function (value, index) {
+    let indexHash = createHash(value);
+    console.log(value + ': ' + indexHash)
+    arrayHash[indexHash] = value;
+});
 
 console.log(arrayHash)
 
